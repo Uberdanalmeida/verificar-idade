c034a603090cacadf3d60bddadbadf634128fdc6
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2026-04-28 22:16:30 -0300

    Atualizando o README.md e ajustando a formatação do código em index.html, script.js e style.css.

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Este projeto consiste em uma aplicação web simples que permite ao usuário ver
 
 ## Funcionalidades
 
-* **Verificação de Idade:** Calcula a idade do usuário com base no ano de nascimento fornecido.
-* **Seleção de Gênero:** Permite ao usuário selecionar entre os gêneros masculino e feminino.
-* **Exibição de Imagem:** Mostra uma imagem representativa da faixa etária e gênero do usuário.
-* **Responsividade:** A aplicação se adapta a diferentes tamanhos de tela, proporcionando uma boa experiência em dispositivos móveis.
-* **Validação de Dados:** Verifica se o ano de nascimento é válido e exibe um alerta caso os dados sejam inválidos.
+- **Verificação de Idade:** Calcula a idade do usuário com base no ano de nascimento fornecido.
+- **Seleção de Gênero:** Permite ao usuário selecionar entre os gêneros masculino e feminino.
+- **Exibição de Imagem:** Mostra uma imagem representativa da faixa etária e gênero do usuário.
+- **Responsividade:** A aplicação se adapta a diferentes tamanhos de tela, proporcionando uma boa experiência em dispositivos móveis.
+- **Validação de Dados:** Verifica se o ano de nascimento é válido e exibe um alerta caso os dados sejam inválidos.
 
 ## Tecnologias Utilizadas
 
-* **HTML:** Estrutura da página web.
-* **CSS:** Estilização da página web, incluindo responsividade.
-* **JavaScript:** Lógica da aplicação, incluindo cálculo da idade, seleção de imagem e validação de dados.
+- **HTML:** Estrutura da página web.
+- **CSS:** Estilização da página web, incluindo responsividade.
+- **JavaScript:** Lógica da aplicação, incluindo cálculo da idade, seleção de imagem e validação de dados.
--- script.js
+++ script.js
@@ -1,68 +1,57 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var txt = document.getElementById('tano')
-    var res = document.getElementById('ver')
+  var data = new Date();
+  var ano = data.getFullYear();
+  var txt = document.getElementById("tano");
+  var res = document.getElementById("ver");
 
-    if(txt.value.length == 0 || txt.value > ano) {
-        alert('Verifique os dados.')
+  if (txt.value.length == 0 || txt.value > ano) {
+    alert("Verifique os dados.");
+  } else {
+    var sexo = document.getElementsByName("sex");
+    var idade = ano - txt.value;
+    var imag = document.getElementById("foto");
+    var genero = "";
+
+    if (sexo[0].checked) {
+      genero = "Homem";
+
+      if (idade >= 0 && idade < 10) {
+        imag.src = "imagens/criança-menino.jpg";
+        res.innerHTML = `Detectamos uma criança de ${idade} anos.`;
+      } else if (idade > 9 && idade < 15) {
+        imag.src = "imagens/menino.jpg";
+        res.innerHTML = `Detectamos um  de ${idade} anos.`;
+      } else if (idade > 14 && idade < 25) {
+        imag.src = "imagens/jovem-homem.jpg";
+        res.innerHTML = `Jovem de ${idade} anos.`;
+      } else if (idade > 24 && idade < 60) {
+        imag.src = "imagens/homem.jpg";
+        res.innerHTML = `${genero} de ${idade} anos.`;
+      } else {
+        imag.src = "imagens/idoso.jpg";
+        res.innerHTML = `${genero} de ${idade} anos.`;
+      }
     }
 
-    else {
-        var sexo = document.getElementsByName('sex')
-        var idade = ano - (txt.value)
-        var imag = document.getElementById('foto')
-        var genero = ''
+    if (sexo[1].checked) {
+      genero = "Mulher";
 
-        if(sexo[0].checked) {
-            genero = 'Homem'
-            
-            if(idade >= 0 && idade < 10) {
-                imag.src = 'imagens/criança-menino.jpg'
-                res.innerHTML = `Detectamos uma criança de ${idade} anos.`
-            }
-            else if(idade > 9 && idade < 15) {
-                imag.src = 'imagens/menino.jpg'
-                res.innerHTML = `Detectamos um  de ${idade} anos.`
-            }
-            else if(idade > 14 && idade < 25) {
-                imag.src = 'imagens/jovem-homem.jpg'
-                res.innerHTML = `Jovem de ${idade} anos.`
-            }
-            else if(idade > 24 && idade < 60) {
-                imag.src = 'imagens/homem.jpg'
-                res.innerHTML = `${genero} de ${idade} anos.`
-            }
-            else {
-                imag.src = 'imagens/idoso.jpg'
-                res.innerHTML = `${genero} de ${idade} anos.`
-            }
-        }
-
-        
-        if(sexo[1].checked) {
-            genero = 'Mulher'
-            
-            if(idade >= 0 && idade < 10) {
-                imag.src = 'imagens/criança-menina.jpg'
-                res.innerHTML = `Criança de ${idade} anos.`
-            }
-            else if(idade > 9 && idade < 15) {
-                imag.src = 'imagens/menina.jpg'
-                res.innerHTML = `Menina de ${idade} anos.`
-            }
-            else if(idade > 14 && idade < 25) {
-                imag.src = 'imagens/jovem-mulher.jpg'
-                res.innerHTML = `Jovem de ${idade} anos.`
-            }
-            else if(idade > 24 && idade < 60) {
-                imag.src = 'imagens/mulher (2).jpg'
-                res.innerHTML = `${genero} de ${idade} anos.`
-            }
-            else {
-                imag.src = 'imagens/idosa.jpg'
-                res.innerHTML = `${genero} de ${idade} anos.`
-            }
-        }
+      if (idade >= 0 && idade < 10) {
+        imag.src = "imagens/criança-menina.jpg";
+        res.innerHTML = `Criança de ${idade} anos.`;
+      } else if (idade > 9 && idade < 15) {
+        imag.src = "imagens/menina.jpg";
+        res.innerHTML = `Menina de ${idade} anos.`;
+      } else if (idade > 14 && idade < 25) {
+        imag.src = "imagens/jovem-mulher.jpg";
+        res.innerHTML = `Jovem de ${idade} anos.`;
+      } else if (idade > 24 && idade < 60) {
+        imag.src = "imagens/mulher (2).jpg";
+        res.innerHTML = `${genero} de ${idade} anos.`;
+      } else {
+        imag.src = "imagens/idosa.jpg";
+        res.innerHTML = `${genero} de ${idade} anos.`;
+      }
     }
+  }
 }
